537e77ac3da4baa4ac17c7c28840ea3b3c5b03ba
chencc33 <chencc3333@gmail.com>
2022-07-29 10:11:29 -0700

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,6 +5,10 @@ const express = require('express')
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+//the check and handleValidationErrors will be used to validate the body of request
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
 // Log in
@@ -54,6 +58,44 @@ router.get(
         }
         // if there is no session, return JSON with an empty obj
         else return res.json({});
+    }
+);
+
+// check whether req.body.credential and req.body.password are empty
+// if empty, return error as response
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
+
+// Log in, connect to the validateLogin middleware
+router.post(
+    '/',
+    validateLogin,
+    async (req, res, next) => {
+        const { credential, password } = req.body;
+
+        const user = await User.login({ credential, password });
+
+        if (!user) {
+            const err = new Error('Login failed');
+            err.status = 401;
+            err.title = 'Login failed';
+            err.errors = ['The provided credentials were invalid.'];
+            return next(err);
+        }
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user
+        });
     }
 );
 
