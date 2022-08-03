a5edf975f64841f029feb87abb1210d3c8d610eb
chencc33 <chencc3333@gmail.com>
2022-08-03 15:05:23 -0700

    create an image for a review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -27,9 +27,43 @@ router.post('/:reviewId/images', restoreUser, async (req, res, next) => {
     const userId = user.toSafeObject().id
     const { url, previewImage } = req.body
     const review = await Review.findByPk(req.params.reviewId)
+    if (!review) {
+        res.status(404)
+        return res.json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        })
+    }
+    const numImage = await Image.count({ where: { reviewId: review.id } })
+    if (numImage >= 10) {
+        res.status(403)
+        return res.json({
+            message: "Maximum number of images for this resource was reached",
+            statusCode: 403
+        })
+    }
     const image = await Image.findOne({
         where: { reviewId: review.id }
     })
+    const newImage = await image.update({
+        url: url,
+        previewImage: previewImage,
+    })
+    let newImageResponse = {
+        id: newImage.id,
+        imageableId: review.id,
+        url: url
+    }
+    res.json(newImageResponse)
+})
+
+// edit a review
+router.put('/:reviewId', restoreUser, async (req, res, next) => {
+    const { user } = req
+    const userId = user.toSafeObject().id
+
+    const review = await Review.findByPk(req.params.reviewId)
     res.json(review)
 })
+
 module.exports = router;
