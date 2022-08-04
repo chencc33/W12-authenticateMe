35a2cb50df8a53446da5ab28b8393379cc4e87a5
chencc33 <chencc3333@gmail.com>
2022-08-04 00:37:58 -0700

    edit a booking

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { Op } = require("sequelize");
 const { User, Spot, Review, Image, Booking, sequelize } = require('../../db/models');
 const router = express.Router();
 
@@ -53,5 +54,71 @@ router.get('/current', restoreUser, async (req, res, next) => {
     res.json({ Bookings: bookingsArr })
 })
 
+// edit a booking
+router.put('/:bookingId', restoreUser, async (req, res, next) => {
+    const { user } = req
+    const userId = user.toSafeObject().id
+
+    const { startDate, endDate } = req.body
+    const editBooking = await Booking.findByPk(req.params.bookingId)
+    // can not find booking
+    if (!editBooking) {
+        res.status(404)
+        res.json({
+            "message": "Booking couldn't be found",
+            "statusCode": 404
+        })
+    }
+    // endDate can not before startdate
+    let startTime = new Date(startDate)
+    let endTime = new Date(endDate)
+    if (endTime <= startTime) {
+        res.status(400)
+        return res.json({
+            "message": "Validation error",
+            "statusCode": 400,
+            "errors": {
+                "endDate": "endDate cannot be on or before startDate"
+            }
+        })
+    }
+    //cant not edit past booking
+    const currentDate = new Date()
+    if (endTime < currentDate) {
+        res.status(403)
+        return res.json({
+            "message": "Past bookings can't be modified",
+            "statusCode": 403
+        })
+    }
+
+    //cant not overlap the booking date of a spot
+    const currentBooking = await Booking.findAll({
+        where: {
+            spotId: editBooking.spotId,
+            [Op.and]: [
+                { startDate: { [Op.lt]: endDate } },
+                { endDate: { [Op.gt]: startDate } }
+            ]
+        }
+    })
+
+    if (currentBooking.length !== 0) {
+        res.status(403)
+        return res.json({
+            "message": "Sorry, this spot is already booked for the specified dates",
+            "statusCode": 403,
+            "errors": {
+                "startDate": "Start date conflicts with an existing booking",
+                "endDate": "End date conflicts with an existing booking"
+            }
+        })
+    }
+    // update the booking
+    await editBooking.update({
+        startDate, endDate
+    })
+    res.json(editBooking)
+})
 
 module.exports = router;
