6a63aae882caea441c42367085e3eca44a33dbde
chencc33 <chencc3333@gmail.com>
2022-08-04 00:55:34 -0700

    delete an image

---
+++ backend/routes/api/images.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
+const { User, Spot, Review, Image, Booking, sequelize } = require('../../db/models');
+const router = express.Router();
+
+router.delete('/:imageId', async (req, res, next) => {
+    const deleteImage = await Image.findByPk(req.params.imageId)
+    if (!deleteImage) {
+        res.status(404)
+        res.json({
+            "message": "Image couldn't be found",
+            "statusCode": 404
+        })
+    } else {
+        res.json({
+            "message": "Successfully deleted",
+            "statusCode": 200
+        })
+    }
+})
+
+
+module.exports = router;
