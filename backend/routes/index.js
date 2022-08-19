ffe2354b5db4c988673d7ba50932c21cdb62ea5f
chencc33 <chencc3333@gmail.com>
2022-08-19 14:12:53 -0700

    setting up express + react application

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,17 +1,9 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-// backend/routes/index.js
-// ...
 const apiRouter = require('./api');
 
-router.use('/api', apiRouter); // all the url in the api router will be prefixed with /api
-// ...
-// // test router
-// router.get('/hello/world', function (req, res) {
-//     res.cookie('XSRF-TOKEN', req.csrfToken());
-//     res.send('Hello World!');
-// });
+router.use('/api', apiRouter);
 
 // Static routes
 // Serve React build files in production
@@ -20,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
     // Serve the frontend's index.html file at the root route
     router.get('/', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
@@ -31,7 +23,7 @@ if (process.env.NODE_ENV === 'production') {
     // Serve the frontend's index.html file at all other routes NOT starting with /api
     router.get(/^(?!\/?api).*/, (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.sendFile(
+        res.sendFile(
             path.resolve(__dirname, '../../frontend', 'build', 'index.html')
         );
     });
@@ -41,17 +33,8 @@ if (process.env.NODE_ENV === 'production') {
 if (process.env.NODE_ENV !== 'production') {
     router.get('/api/csrf/restore', (req, res) => {
         res.cookie('XSRF-TOKEN', req.csrfToken());
-        return res.json({});
+        res.status(201).json({});
     });
 }
-
-// allow any developer to re-set the CSRF-token cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-        'XSRF-Token': csrfToken
-    });
-});
 
 module.exports = router;
