c81dc868afe15d4ebeb247724cc5ed2086673fc2
chencc33 <chencc3333@gmail.com>
2022-08-20 21:47:44 -0700

    incomplete get all spots

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import sessionReducer from './session';
 import thunk from "redux-thunk";
+import { spotReducer } from "./spot";
 
 const rootReducer = combineReducers({
   // add reducer functions here
   session: sessionReducer,
+  spots: spotReducer
 });
 
 let enhancer;
