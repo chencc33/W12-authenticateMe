abb6ea4558e8a3357522233212db457305d7c34a
chencc33 <chencc3333@gmail.com>
2022-08-21 00:04:32 -0700

    show spots finish

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import sessionReducer from './session';
 import thunk from "redux-thunk";
-import { spotReducer } from "./spot";
+import spotReducer from "./spot";
 
 const rootReducer = combineReducers({
   // add reducer functions here
