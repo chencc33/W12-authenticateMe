3e3fc97b9e9630c05442834ac861cea8ef46a863
chencc33 <chencc3333@gmail.com>
2022-08-24 12:38:48 -0700

    change image type

--- backend/db/migrations/20220801210620-create-spot.js
+++ backend/db/migrations/20220801210620-create-spot.js
@@ -39,7 +39,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       previewImage: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
