824b877d57727c6684625ddb1b3b7aac3bcf27c8
chencc33 <chencc3333@gmail.com>
2022-08-26 13:23:01 -0700

    add validation errors for review form

--- frontend/src/components/Review/ReviewListBySpot.js
+++ frontend/src/components/Review/ReviewListBySpot.js
@@ -5,6 +5,7 @@ import { getReviewBySpot } from '../../store/review'
 import './Review.css'
 import { createReview } from '../../store/review'
 import ReviewDetail from './ReviewDetail'
+import './CreateReviewForm.css'
 
 const ReviewListBySpot = () => {
     const { spotId } = useParams()
@@ -19,6 +20,8 @@ const ReviewListBySpot = () => {
     const [review, setReview] = useState("")
     const [stars, setStars] = useState(0)
     const [showForm, setShowForm] = useState(false)
+    const [validationErrors, setValidationErrors] = useState([])
+    const [hasSubmitted, setHasSubmitted] = useState(false)
 
     // console.log('****from component****', reviewsArr)
 
@@ -37,13 +40,27 @@ const ReviewListBySpot = () => {
     // console.log('******showButton******', showButton)
     const handleSubmit = async (e) => {
         const data = { review, stars }
+        setHasSubmitted(true)
         await dispatch(createReview(data, spotId))
     }
+
+    useEffect(() => {
+        let errors = []
+        if (!review.length) errors.push('Please enter your review')
+        setValidationErrors(errors)
+    }, [review])
 
     return (
         <>
             <div>
-                {reviewsArr.length && (
+                {validationErrors.length > 0 && hasSubmitted && (
+                    <ul>
+                        {validationErrors.map((error) => (
+                            <li key={error}>{error}</li>
+                        ))}
+                    </ul>
+                )}
+                {reviewsArr.length > 0 && (
                     <div>
                         {reviewsArr.map((review) => (
                             <div key={review.id} className='singe-review-box' onClick={() => { history.push(`/reviews/${review.id}`) }}>
@@ -61,12 +78,12 @@ const ReviewListBySpot = () => {
             <div>
                 {showButton && !reviewsArr.length && (
                     <>
-                        <div>
+                        <div className='create-review-instruction'>
                             There is no review yet. Want to create one?
                         </div>
                         <div hidden={showForm} onClick={() => setShowForm(true)} className='button'>create</div>
                         {showForm && (
-                            <form>
+                            <form className='form-box'>
                                 <label>
                                     review
                                     <input
@@ -91,7 +108,7 @@ const ReviewListBySpot = () => {
             </div>
             <div>
 
-                {showButton && reviewsArr.length && (
+                {showButton && reviewsArr.length > 0 && (
                     <>
                         <div hidden={showForm} onClick={() => setShowForm(true)} className='button'>create</div>
                         {showForm && (
@@ -112,7 +129,7 @@ const ReviewListBySpot = () => {
                                         onChange={(e) => setStars(e.target.value)}
                                     />
                                 </label>
-                                <div onClick={handleSubmit} className='button'>submit</div>
+                                <div onClick={handleSubmit} className='form-button'>submit</div>
                             </form>
                         )}
                     </>
