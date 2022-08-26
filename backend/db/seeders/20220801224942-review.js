894c8b11590faad12098c6ee0d4f41fb8accef7d
chencc33 <chencc3333@gmail.com>
2022-08-25 20:16:05 -0700

    remigrate seeder file

--- backend/db/seeders/20220801224942-review.js
+++ backend/db/seeders/20220801224942-review.js
@@ -15,14 +15,38 @@ module.exports = {
       {
         userId: 1,
         spotId: 1,
-        review: "This was an awesome spot!",
+        review: "This house is gorgeous! Beautiful inside and out! Private but so close to downtown. It was perfect for our girls getaway",
         stars: 5,
       },
       {
         userId: 1,
         spotId: 2,
-        review: "This was a okay spot!",
+        review: "Gorgeous location and great house. Would stay again!",
         stars: 4,
+      },
+      {
+        userId: 2,
+        spotId: 1,
+        review: "Great place! We really enjoyed our stay!",
+        stars: 4,
+      },
+      {
+        userId: 2,
+        spotId: 3,
+        review: "You will not be disappointed! This ranch is meticulously maintained and creates an amazing atmosphere to relax and recharge.",
+        stars: 4,
+      },
+      {
+        userId: 2,
+        spotId: 4,
+        review: "Had the best time! It was great to be able to be able to relax in one of Mary’s casitas! The night sky was beautiful and the casita itself was so cute and comfortable! Will definitely be back!",
+        stars: 4,
+      },
+      {
+        userId: 2,
+        spotId: 5,
+        review: "This cabin is in a nice location that provides a lot of privacy even though there are houses close by. There's a fenced yard which is terrific for pets. The interior is lovely with quality decorations. ",
+        stars: 5,
       }
     ])
   },
